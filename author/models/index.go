dc67bb042a7d7f23bff7735c45b27a56fd9b5719
RaffaRaihan <kaizyexercise@gmail.com>
2024-09-30 16:27:21 +0700

    "connect to database postgres"

--- author/models/index.go
+++ author/models/index.go
@@ -1,27 +1,18 @@
 package models
 
 import (
-	"fmt"
 	"os"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func GetSqlConnection() (*gorm.DB, error) {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s database=%s port=%s TimeZone=Asia/Jakarta",
-		os.Getenv("PG_HOST"),
-		os.Getenv("PG_USER"),
-		os.Getenv("PG_PASS"),
-		os.Getenv("PG_DB"),
-		os.Getenv("PG_DB"),
-		os.Getenv("PG_PORT"),
-	)
-
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	dsn := os.Getenv("DATABASE_URL")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
-		return nil, err
+		panic("Gagal Menyambung Ke Database")
 	}
 
 	return db, nil
