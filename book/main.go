8c486099ca2f088929e6683c7ac194bda37c48f1
RidhoAnjarM <anjarridho081@gmail.com>
2024-10-01 10:36:42 +0700

    buat server main

--- book/main.go
+++ book/main.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"sistem-microservice/book/constants"
+	"sistem-microservice/book/controllers"
+
+	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	postgresURL := os.Getenv("POSTGRES_URL")
+	if postgresURL == "" {
+		log.Fatal("POSTGRES_URL not set in .env file")
+	}
+
+	db, err := pgxpool.Connect(context.Background(), postgresURL)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v", err)
+	}
+	defer db.Close()
+
+	fmt.Println("Connected to database")
+
+	listener, err := net.Listen("tcp", ":"+constants.PORT)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	fmt.Printf("Server is running on port %s\n", constants.PORT)
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
